34037c40bafb6122420fce36f850000ef19f2913
kanadome <tsuchiyakeita2025@gmail.com>
2024-08-30 21:28:33 +0900

    サンプルデータを変更

--- 要件定義/insert.sql
+++ 要件定義/insert.sql
@@ -3,36 +3,42 @@ INSERT INTO
   users (auth_id, username, avatar)
 VALUES
   (
-    'd0b521e7-7993-4f94-8993-34ad3329ea8d',
-    'user1',
-    'https://example.com/avatar1.png'
+    '0e060f78-1560-4877-9112-87120e78510b',
+    'ガネーシャ',
+    'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/icons/icon1.jpg'
   ),
   (
-    'b412483d-4809-4ac9-b4b7-580ae2621594',
-    'user2',
-    'https://example.com/avatar2.png'
+    '5424b27c-0101-4a4a-9be3-51c12532d074',
+    '土屋',
+    'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/icons/icon2.jpg'
+  ),
+  (
+    '8593b706-d74b-4dc8-9258-ddf4291a0ef5',
+    'kasiteto',
+    'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/icons/icon3.png?t=2024-08-30T11%3A57%3A07.670Z'
   );
 
 -- videosテーブル
-INSERT INTO videos (user_id, title, description, video_url, thumbnail_url) VALUES
-    (16, 'サンプル動画1', 'これは動画1の説明です', 'https://example.com/video1', 'https://example.com/thumbnail1.jpg'),
-    (16, 'サンプル動画2', 'これは動画2の説明です', 'https://example.com/video2', 'https://example.com/thumbnail2.jpg'),
-    (17, 'サンプル動画3', 'これは動画3の説明です', 'https://example.com/video3', 'https://example.com/thumbnail3.jpg');
+INSERT INTO videos (user_id, title, description, video_url, manim_url, thumbnail_url, views, article_url) VALUES
+    (1, 'サンプル動画1', 'これは動画1の説明です', 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/videos/displayEquations.mp4', 'https://example.com/manim1.py', 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/thumbneils/thumbnail2.png?t=2024-08-30T12%3A10%3A43.263Z', 100, 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/articles/samplearticle.md?t=2024-08-30T12%3A19%3A27.231Z'),
+    (2, 'サンプル動画2', 'これは動画2の説明です', 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/videos/CreateCircle.mp4', 'https://example.com/manim2.py', 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/thumbneils/thumbnail3.png?t=2024-08-30T12%3A11%3A04.055Z', 150, 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/articles/samplearticle.md?t=2024-08-30T12%3A19%3A27.231Z'),
+    (3, 'サンプル動画3', 'これは動画3の説明です', 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/videos/displayTextJP.mp4', 'https://example.com/manim3.py', 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/thumbneils/thumbnail.png?t=2024-08-30T12%3A11%3A12.010Z', 200, 'https://slthsjneofwnmtffmcer.supabase.co/storage/v1/object/public/articles/samplearticle.md?t=2024-08-30T12%3A19%3A27.231Z');
 
 -- likesテーブル
 INSERT INTO likes (user_id, video_id) VALUES
-    (16, 7),
-    (16, 8),
-    (17, 9),
-    (17, 7)
+    (1, 2),
+    (2, 3),
+    (3, 1),
+    (3, 2),
+    (3, 3);
 
 -- commentsテーブル
 INSERT INTO comments (user_id, video_id, content) VALUES
-    (16, 7, 'すばらしい動画です！'),
-    (16, 7, 'とても参考になりました。'),
-    (17, 8, 'この動画が本当に気に入りました！'),
-    (17, 8, '説明がわかりやすいです！'),
-    (17, 9, 'コンテンツが素晴らしかったです！');
+    (1, 1, 'すばらしい動画です！'),
+    (1, 2, 'とても参考になりました。'),
+    (2, 3, 'この動画が本当に気に入りました！'),
+    (2, 1, '説明がわかりやすいです！'),
+    (3, 2, 'コンテンツが素晴らしかったです！');
 
 -- tagsテーブル
 INSERT INTO tags (name) VALUES
@@ -44,20 +50,21 @@ INSERT INTO tags (name) VALUES
 
 -- video_tagsテーブル
 INSERT INTO video_tags (video_id, tag_id) VALUES
-    (7, 1),
-    (7, 3),
-    (7, 2),
-    (8, 4),
-    (9, 5);
+    (1, 1),
+    (1, 3),
+    (2, 2),
+    (2, 4),
+    (3, 5);
 
 -- reference_itemsテーブル
 INSERT INTO reference_items (title, url) VALUES
-    ('参考資料1', 'https://example.com/reference1'),
-    ('参考資料2', 'https://example.com/reference2'),
-    ('参考資料3', 'https://example.com/reference3');
+    ('参考資料1', 'https://zenn.dev/k41531/articles/828e81c86560ae'),
+    ('参考資料2', 'https://zenn.dev/decl/articles/8d5c80a2331be0'),
+    ('参考資料3', 'https://qiita.com/ochimusha01/items/7f6685a1034fee00c6bd');
 
 -- video_referencesテーブル
 INSERT INTO video_references (video_id, reference_id) VALUES
-    (7, 1),
-    (8, 2),
-    (9, 3);
+    (1, 1),
+    (2, 2),
+    (3, 3),
+
